93025b02e620d25a0bce35a4f5448635593de8c7
Lâm Hoàng Thanh <132256188+thanh9460413@users.noreply.github.com>
2023-07-22 17:36:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,58 @@ const printBtn = document.getElementById('print-btn');
 const khachHangInput = document.getElementById('kh-input');
 const SDTkhachHangInput = document.getElementById('sdt-input');
 const DiaChikhachHangInput = document.getElementById('diachi-input');
+const firebaseConfig = {
+  apiKey: "AIzaSyAL2kP_r7MofPUadyxQZytIpF0CgQxcUMI",
+  authDomain: "gomnhatyenvan.firebaseapp.com",
+  databaseURL: "https://gomnhatyenvan-default-rtdb.firebaseio.com",
+  projectId: "gomnhatyenvan",
+  storageBucket: "gomnhatyenvan.appspot.com",
+  messagingSenderId: "106663547032",
+  appId: "1:106663547032:web:5f27df39c877728a71c0ed",
+  measurementId: "G-ZV70FXJ02J"
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const itemsRef = database.ref("/Items");
+let foundItem = null;
+// Hàm tìm kiếm số điện thoại trong mảng dữ liệu
+function searchSdtInArray(allItems, searchSdt) {
+
+  for (const key in allItems) {
+    if (allItems[key].SDTKhachHang === searchSdt) {
+      foundItem = allItems[key];
+      break;
+    }
+  }
+  return foundItem;
+}
+// Lắng nghe sự kiện "input" trên ô input số điện thoại
+SDTkhachHangInput.addEventListener('input', function () {
+  const searchSdt = SDTkhachHangInput.value;
+
+  // Lấy tất cả dữ liệu trong nút "Items"
+  itemsRef.once('value')
+    .then(snapshot => {
+      const allItems = snapshot.val();
+      const foundItem = searchSdtInArray(allItems, searchSdt);
+      if (foundItem) {
+        // Nếu tìm thấy số điện thoại, hiển thị thông tin tương ứng lên input
+        khachHangInput.value = foundItem.KhachHang;
+        DiaChikhachHangInput.value = foundItem.DiaChiKhachHang;
+      }
+    })
+
+    .catch(error => {
+      console.error("Error getting all items:", error);
+    });
+});
 
 let stt = 0;
 let totalAmount = 0;
 let khachHang = '';
 let SDTKhachhang = '';
 let DiaChiKhachhang = '';
-addBtn.addEventListener('click', function() {
+addBtn.addEventListener('click', function () {
   const table = document.getElementById('report-table');
   const tbody = table.querySelector('tbody');
   const newRow = document.createElement('tr');
@@ -19,29 +64,28 @@ addBtn.addEventListener('click', function() {
   const sttCell = document.createElement('td');
   sttCell.textContent = ++stt;
   newRow.appendChild(sttCell);
-// Tạo các ô mới và thêm dữ liệu nhập vào
-let amount = 0;
-for (let i = 3; i < inputFields.length; i++) {
-  const cell = document.createElement('td');
-  if (i === 3) {
-    cell.textContent = inputFields[i].value;
-  } else if (i === 5) {
-    amount = parseFloat(inputFields[i].value.replace(/\./g, ''));
-    const formattedAmount = amount.toLocaleString('en-US', { minimumFractionDigits: 0 });
-    cell.textContent = formattedAmount.replace(/,/g, '.') + '.000';
-  } else {
-    cell.textContent = inputFields[i].value;
+  // Tạo các ô mới và thêm dữ liệu nhập vào
+  let amount = 0;
+  for (let i = 3; i < inputFields.length; i++) {
+    const cell = document.createElement('td');
+    if (i === 3) {
+      cell.textContent = inputFields[i].value;
+    } else if (i === 5) {
+      amount = parseFloat(inputFields[i].value.replace(/\./g, ''));
+      const formattedAmount = amount.toLocaleString('en-US', { minimumFractionDigits: 0 });
+      cell.textContent = formattedAmount.replace(/,/g, '.') + '.000';
+    } else {
+      cell.textContent = inputFields[i].value;
+    }
+    newRow.appendChild(cell);
   }
-  newRow.appendChild(cell);
-}
-
 
 
   // Tạo nút x và gắn sự kiện xóa item khi nhấp vào nút x
   const deleteBtn = document.createElement('button');
   deleteBtn.innerHTML = 'x';
   deleteBtn.classList.add(".close");
-  deleteBtn.addEventListener('click', function() {
+  deleteBtn.addEventListener('click', function () {
     newRow.remove();
     updateSttAndTotalAmount();
   });
@@ -66,7 +110,7 @@ for (let i = 3; i < inputFields.length; i++) {
 });
 
 
-printBtn.addEventListener('click', function() {
+printBtn.addEventListener('click', function () {
   khachHang = khachHangInput.value;
   SDTKhachhang = SDTkhachHangInput.value;
   DiaChiKhachhang = DiaChikhachHangInput.value;
@@ -93,12 +137,13 @@ printBtn.addEventListener('click', function() {
       tableData.push(rowData);
     }
   }
-// Sắp xếp lại các dòng trong mảng tableData
+
+  // Sắp xếp lại các dòng trong mảng tableData
   tableData.reverse();
   const docDefinition = {
     // pageOrientation: 'landscape',
     content: [
-      { text: `Gốm Nhật Yến Vân`, style: 'header' },
+      { text: `Gốm Nhật Yến Vân`, style: 'header', fontSize: 20 },
       { text: `SĐT/Zalo: 0918095223 & 0919696242` },
       { text: `Địa chỉ: 271 An Dương Vương Phường An Lạc Quận Bình Tân TpHCM` },
       { text: `Tên Khách Hàng: ${khachHang}` },
@@ -130,7 +175,60 @@ printBtn.addEventListener('click', function() {
       }
     }
   };
+  const searchSdt = SDTKhachhang;
+  const newKh = khachHang;
+  const newDiachi = DiaChiKhachhang;
 
+  // Lấy tất cả dữ liệu trong nút "Items"
+  itemsRef.once('value')
+    .then(snapshot => {
+      const allItems = snapshot.val();
+      let foundItemId = null;
+
+      for (const key in allItems) {
+        if (allItems[key].SDTKhachHang === searchSdt) {
+          foundItemId = key;
+          break;
+        }
+      }
+
+      if (foundItemId) {
+        // Nếu đã tồn tại dữ liệu với số điện thoại đã nhập
+        const existingKh = allItems[foundItemId].KhachHang;
+        const existingDiachi = allItems[foundItemId].DiaChiKhachHang;
+
+        // Kiểm tra xem dữ liệu đã nhập có khác với dữ liệu hiện tại trong Firebase hay không
+        if (newKh !== existingKh || newDiachi !== existingDiachi) {
+          // Nếu khác, thực hiện cập nhật dữ liệu mới lên Firebase
+          itemsRef.child(foundItemId).update({
+            KhachHang: newKh,
+            DiaChiKhachHang: newDiachi
+          }).then(() => {
+            console.log("Update thông tin thành công!");
+          }).catch(error => {
+            console.error("Lỗi khi update thông tin:", error);
+          });
+        } else {
+          // Nếu giống, không thực hiện bất kỳ hành động nào
+          console.log("Dữ liệu giống nhau, không cần thay đổi.");
+        }
+      } else {
+        // Nếu không tồn tại dữ liệu với số điện thoại đã nhập, thực hiện thêm mới
+        const newItemRef = database.ref("/Items/").child(searchSdt);
+        newItemRef.set({
+          SDTKhachHang: searchSdt,
+          KhachHang: newKh,
+          DiaChiKhachHang: newDiachi
+        }).then(() => {
+          console.log("Thêm mới thành công!");
+        }).catch(error => {
+          console.error("Lỗi khi thêm mới:", error);
+        });
+      }
+    })
+    .catch(error => {
+      console.error("Error getting all items:", error);
+    });
   const pdfDocGenerator = pdfMake.createPdf(docDefinition);
   pdfDocGenerator.download('phieu_bao_cao.pdf');
 });
@@ -162,14 +260,14 @@ const navbarToggle = document.querySelector('.navbar-toggler');
 const navbarNav = document.querySelector('#navbarNav');
 
 // Xử lý sự kiện khi nhấp vào nút toggle navbar
-navbarToggle.addEventListener('click', function() {
+navbarToggle.addEventListener('click', function () {
   navbarNav.classList.toggle('show');
 });
 
 // Xử lý sự kiện khi chọn một liên kết trong navbar
 const navLinks = navbarNav.getElementsByTagName('a');
 for (let i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener('click', function() {
+  navLinks[i].addEventListener('click', function () {
     // Chuyển trang khi nhấp vào liên kết
     window.location.href = this.getAttribute('href');
   });
